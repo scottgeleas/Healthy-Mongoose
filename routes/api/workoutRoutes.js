2c07d554b5c2ec3212bf197fa12f5fb189bef0c2
Scott Geleas <scottgeleas97@gmail.com>
2021-07-09 14:15:33 -0400

    site wont runstill

--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -2,41 +2,43 @@ const express = require('express');
 const router = express.Router();
 const Workout = require('../../models/workout');
 
-// GET all workouts/last workout
+// GET last workout all workouts
 router.get('/workouts', (req, res) => {
     try {
         Workout.find({})
-            // Workout.aggregate(
-            //     [{
-            //         "$addFields": {
-            //             "totalDuration": { "$sum": "$exercises.duration" }
-            //         }
-            //     }],
-            //     function(err, results) {
-            //         res.json(results)
-            //     }
-            // );
-    } catch (err) {
-        res.json(err);
-    };
-});
 
-// GET all workouts stats 7 days
-router.get('/workouts/range', (req, res) => {
-    try {
-        Workout.find({})
         Workout.aggregate(
             [{
                 "$addFields": {
                     "totalDuration": { "$sum": "$exercises.duration" }
-                },
-                "$limit": 7
+                }
             }],
             function(err, results) {
                 res.json(results)
             }
         )
 
+    } catch (err) {
+        res.json(err);
+    };
+});
+
+// GET duration of all workouts
+router.get('/workouts/range', (req, res) => {
+    try {
+        Workout.find({})
+
+
+        Workout.aggregate(
+            [{
+                "$addFields": {
+                    "totalDuration": { "$sum": "$exercises.duration" }
+                }
+            }],
+            function(err, results) {
+                res.json(results)
+            }
+        );
     } catch (err) {
         res.json(err);
     };
